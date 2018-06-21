88641017caa9bbfc43dca86b19f47604460a4fc6
Gerald Versluis <gerald@verslu.is>
2018-06-21 10:22:43 +0200

    Initial commit

---
+++ TwoWayBindingTest/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TwoWayBindingTest
+{
+	public class LoginViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		private string _email;
+		public string Email
+		{
+			get => _email;
+			set
+			{
+				_email = value;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(Email)));
+			}
+		}
+	}
+}
